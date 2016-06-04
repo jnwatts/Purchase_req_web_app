df49bd3273ba6fc93ac05c265e74f7a1f0906e2f
Josh Watts <josh+github@sroz.net>
2016-06-04 08:49:42 -0400

    api: Implement Models/Role

--- api/src/Models/Roles.php
+++ api/src/Models/Roles.php
@@ -2,20 +2,67 @@
 namespace PurchaseReqs\Models;
 if (!defined('PURCHASE_REQS')) { die('Oops!'); }
 
-class Roles extends Model {
-	private $roles = [
-		Roles::Role(1, "Admin");
-	];
+class Roles extends \PurchaseReqs\Model {
+	const ADMIN = 1;
+	const PM = 2;
+	const ACCT = 3;
+	const MGMT = 4;
 
-	static Role(id, description) {
+	private $roles = [];
+
+	private static function Role($id, $description) {
 		return [
-			'id' => id,
-			'description' => description,
+			'id' => $id,
+			'description' => $description,
 		];
 	}
 
 	public function __construct($ci) {
+        $this->tables = [
+            'user_role',
+        ];
+
 		parent::__construct($ci);
+
+		$this->roles = [
+			Roles::Role(Roles::ADMIN, "Admin"),
+			Roles::Role(Roles::PM, "Project Manager"),
+			Roles::Role(Roles::ACCT, "Accounting"),
+			Roles::Role(Roles::MGMT, "Management"),
+		];
+	}
+
+
+	public function rolesFromUser($user_id) {
+		$db = $this->db();
+		$user_roles = $db->select('user_role', '*', ['user_role.user_id' => $user_id]);
+		$this->checkDbError($db);
+
+		$roles = [];
+
+		foreach ($user_roles as $user_role) {
+			$roles[] = $user_role['role_id'];
+		}
+
+		return $roles;
+	}
+
+	public function usersWithRole($role_id) {
+		$user_roles = $db->select('user_role', '*', ['user_role.role_id' => $role_id]);
+		$this->checkDbError($db);
+
+		$users = [];
+
+		foreach ($user_roles as $user_role) {
+			$users[] = $user_role['user_id'];
+		}
+
+		return $users;
+	}
+
+	public function byId($role_id) {
+		assert($role_id < count($this->roles));
+		return $this->roles[$role_id];
 	}
 }
 
